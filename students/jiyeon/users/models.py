20c04306fb9225d3102c7624e69d6901be2f15d4
Jiyeon Kim <jiyeonkim@Jiyeonui-MacBookPro.local>
2022-01-13 16:34:24 +0900

    [Fix] Apply coding convention

--- students/jiyeon/users/models.py
+++ students/jiyeon/users/models.py
@@ -1,14 +1,12 @@
 from django.db import models
-from django.db.models.deletion import CASCADE
-
 
 class User(models.Model):
-    name = models.CharField(max_length=30)
-    email = models.CharField(max_length=200, unique=True)
-    password = models.CharField(max_length=30)
+    name         = models.CharField(max_length=30)
+    email        = models.CharField(max_length=200, unique=True)
+    password     = models.CharField(max_length=200)
     phone_number = models.CharField(max_length=30)
+    created_at   = models.DateTimeField(auto_now_add=True)
+    updated_at   = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = 'users'
-
-
