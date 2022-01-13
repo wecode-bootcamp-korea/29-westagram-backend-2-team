061c438a2ce07e98fe29d488efc736b80b3969c0
JH Ms <jhms@JHs-MacBook-Air.local>
2022-01-13 13:30:49 +0900

    [지현] : Modeling

---
+++ students/jihyun/westagram/users/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+
+# Create your models here.
+class Name(models.Model) :
+    name = models.CharField(max_length=50)
+
+    class Meta :
+        db_table = 'names'
+
+class Email(models.Model) :
+    address = models.CharField(max_length=100)
+    name = models.ForeignKey(Name, on_delete=models.CASCADE)
+
+    class Meta :
+        db_table = 'emails'
+
+class Password(models.Model) :
+    password = models.CharField(max_length=20)
+    email = models.ForeignKey(Email, on_delete=models.CASCADE)
+
+    class Meta :
+        db_table = 'passwords'
+
+class Phonenumber(models.Model) :
+    phonenumber = models.CharField(max_length=20)
+    name = models.ForeignKey(Name, on_delete=models.CASCADE)
+
+    class Meta :
+        db_table = 'phonenumbers'
+
+
+
+
+
+
+
+phonenumber
