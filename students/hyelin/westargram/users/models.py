5d124548233681412b4058d4cf32d360353f63d5
OolongTea620 <rlafls9596@gmail.com>
2022-01-14 16:53:27 +0900

    add created_at, updated_at colunm

--- students/hyelin/westargram/users/models.py
+++ students/hyelin/westargram/users/models.py
@@ -6,6 +6,8 @@ class User(models.Model):
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=300)
     phone = models.CharField(unique=True, max_length=120)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta :
         db_table = 'users'
