598e9e612a41f57937fb43af645993a09faab606
Jiyeon Kim <jiyeonkim@Jiyeonui-MacBookPro.local>
2022-01-14 17:43:02 +0900

    [Fix] change code for signup

--- students/jiyeon/users/views.py
+++ students/jiyeon/users/views.py
@@ -3,6 +3,7 @@ import re
 
 from django.views import View
 from django.http  import JsonResponse
+from django.db import IntegrityError
 
 from .models import User
 
@@ -31,31 +32,34 @@ class UserView(View):
         """
         data = json.loads(request.body)
 
-        regex_email    = re.compile(r'^[a-zA-Z0-9][a-zA-Z0-9._]+[@][a-zA-Z][A-Za-z.]+[.]\w{2,}')
-        regex_password = re.compile(r'^(?=.*\w)(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$')
-
-        try:
-            filtered_query = User.objects.get(email=data['email'])
-            return JsonResponse({"Duplicate Email" : "이메일이 중복되었습니다."})   
-        except User.DoesNotExist:
-            pass
-   
-
-        if re.fullmatch(regex_email, data['email']) == None: # 이메일 양식에 맞게 입력 되었는지 확인
+        REGEX_EMAIL    = r'^[a-zA-Z0-9][a-zA-Z0-9._]+[@][a-zA-Z][A-Za-z.]+[.]\w{2,}'
+        REGEX_PASSWORD = r'^(?=.*\w)(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$'
+        
+        if not re.fullmatch(REGEX_EMAIL, data['email']): # 이메일 양식에 맞게 입력 되었는지 확인
             
             return JsonResponse({"E-mail Error": "양식에 맞는 메일 주소를 입력해주세요."})
         
-        if re.fullmatch(regex_password, data['password']) == None: # 비밀번호 조건에 맞게 입력되었는지 확인
+        if not re.fullmatch(REGEX_PASSWORD, data['password']): # 비밀번호 조건에 맞게 입력되었는지 확인
             
             return JsonResponse(
                 {"Password Error": "8자리 이상의 알파벳, 숫자, 특수문자(@$!%*?&)를 포함한 비밀번호를 입력해주세요."}
                 )
 
-        user = User.objects.create(
+        try:
+            user, created = User.objects.get_or_create(
             name         = data['name'],
             email        = data['email'],
             password     = data['password'],
-            phone_number = data['password']
-        )
+            phone_number = data['phone_number']
+            )
+        except User.DoesNotExist:
+            return JsonResponse({"E-mail Error": f"중복된 이메일로 가입을 시도했습니다."})
+        except IntegrityError:
+            return JsonResponse({"E-mail Error": f"중복된 이메일로 가입을 시도했습니다."})
 
-        return JsonResponse({"message" : f"사용자 등록 성공, {data['email']}"}, status=201)
+        
+        if created == True:
+            return JsonResponse({"message" : f"사용자 등록 성공, {data['email']}"}, status=201)
+
+        else:
+            return JsonResponse({"E-mail Error": "이미 가입된 회원입니다."})
