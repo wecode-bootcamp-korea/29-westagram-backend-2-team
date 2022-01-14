805f5f5bd8f3ba4e2f4809585f40f34ec557b9f1
JH Ms <jhms@JHs-MacBook-Air.local>
2022-01-14 14:12:14 +0900

    [지현]Modeling

--- students/jihyun/westagram/users/models.py
+++ students/jihyun/westagram/users/models.py
@@ -2,10 +2,9 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model) :
-    name = models.CharField(max_length=50)
-    address = models.CharField(max_length=100)
-    password = models.CharField(max_length=20)
-    email = models.ForeignKey(Email, on_delete=models.CASCADE)
+    name        = models.CharField(max_length=50)
+    address     = models.EmailField(Unique=True)
+    password    = models.CharField(max_length=200)
     phonenumber = models.CharField(max_length=20)
 
     class Meta :
