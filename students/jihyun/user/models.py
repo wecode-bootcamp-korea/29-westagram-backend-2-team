caa8b343f8410e078289915e1b02099bd3ae3f9b
JH Ms <jhms@JHs-MacBook-Air.local>
2022-01-18 14:30:27 +0900

    [지현] Modeling

--- students/jihyun/user/models.py
+++ students/jihyun/user/models.py
@@ -3,9 +3,11 @@ from django.db import models
 # Create your models here.
 class User(models.Model) :
     name        = models.CharField(max_length=50)
-    address     = models.EmailField(Unique=True)
+    email       = models.EmailField(Unique=True)
     password    = models.CharField(max_length=200)
     phonenumber = models.CharField(max_length=20)
+    created_at  = models.DateTimeField(auto_now_add=True)
+    updated_at  = models.DateTimeField(auto_now=True)
 
     class Meta :
         db_table = 'users'
