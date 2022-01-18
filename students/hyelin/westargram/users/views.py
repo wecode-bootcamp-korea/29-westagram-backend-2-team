ead04e04e9500e49f21fb42b794fd05711ebcdf2
OolongTea620 <rlafls9596@gmail.com>
2022-01-18 11:21:44 +0900

    sign_up

--- students/hyelin/westargram/users/views.py
+++ students/hyelin/westargram/users/views.py
@@ -1,6 +1,5 @@
-import json #json형태의 객체용
-import re #정규식 연산용
-import bcrypt #암호화
+import json 
+import re 
 ##View
 from django.views import View
 ##Model
@@ -13,9 +12,9 @@ class SignUpView (View):
     def post(self, request):
         
         user_data   = json.loads(request.body)
-        
-        REX_EMAIL   = r'^[0-9A-Za-z][0-9A-Za-z._]+[@][A-Za-z][A-Za-z.]+[.]\w{2,}'
-        REX_PASS    = r'^(?=.*\w)(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$'
+        print(user_data +'::'+ type(user_data) )#임시객체 확인용,
+        REX_EMAIL   = r'^[a-zA-Z0-9][a-zA-Z0-9_]+[@][a-zA-Z0-9][A-Za-z.]+[.]\w{2,}'
+        REX_PASS    = r'^(?=.*\w)(?=.*\d)(?=.*[@$!%*?&])[a-zA-Z\d@$!%*?&]{8,}$'
         
         ## 1. name누락 
         if 'name' not in user_data :
@@ -34,35 +33,12 @@ class SignUpView (View):
             return JsonResponse({'message' : '잘못된 형식의 비밀번호 입니다'}, status=401)
         ## 6. 이메일 중복시 -> 이미 가입된 email입니다
         if not User.objects.filter(email = user_data['email']).exists() :
-            password    = user_data['password'].encode('utf-8')
-            hash_pass   = bcrypt.hashpw(password, bcrypt.getsalt())
             User.objects.create(
                 name = user_data['name'],
-                password = hashed_password,
+                password = user_data['password'],
                 email = user_data['email'],
                 phone = user_data['phone']        
             )
-            
             return JsonResponse({'message' : 'SUCCESS'}, status=201)
         
         return JsonResponse({'message' : '이미 가입된 회원 입니다'}, status=401)
-
-        
-        
-
-class LoginView (View):    
-    ## 이메일 입력 누락시 
-    ## PW입력 누락시
-    ## email맞, pw틀
-    ## email 존재 X 
-    ##  올바른 email과 pw
-
-    def post(self,request):
-        
-        return JsonResponse({'message' : '이메일을 입력해 주세요'}, status=400)
-        
- 
-class LogoutView (View):
-    ## 세션이 들어오면 :expired 처리하기
-    def post(self, request):
-        return JsonResponse({'message' : '로그아웃처리 완료'}, status=200)
