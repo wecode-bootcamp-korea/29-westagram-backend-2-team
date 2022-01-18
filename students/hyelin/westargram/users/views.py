9f5e7ad326fb7b5cad6408f7f0ea1b67dbd8ec14
OolongTea620 <rlafls9596@gmail.com>
2022-01-18 15:03:38 +0900

    login util make

--- students/hyelin/westargram/users/views.py
+++ students/hyelin/westargram/users/views.py
@@ -54,7 +54,7 @@ class SignUpView (View):
                 name = user_data['name'],
                 password = user_data['password'],
                 email = user_data['email'],
-                phone = user_data['phone']        
+                phone = user_data['phone']  
             )
             return JsonResponse({'message' : 'SUCCESS'}, status=201)
         
@@ -62,18 +62,34 @@ class SignUpView (View):
 
 class LoginView(View):
     def post(self, request):
+
+        """
+        1. 목적 : 클라이언트로부터 받은 로그인 목적의 사용자 데이터
+        (이메일과 비밀번호를 데이터베이스에 조회하여 일치하는지 확인하고  
+        일치 여부에 따라 로그인 성공/실패를 결정)
+        2. INPUT : 
+        {
+            "email"        : "rlafls9596@gamil.com",
+            "password"     : "rlagPFls95@",
+        }
+        3. OUTPUT : 
+        성공 시
+        {
+            "message" : "SUCCESS" , status=201
+        }
+        """
+
         data = json.loads(request.body)
 
         if 'email' not in data  or 'password' not in data :
             return JsonResponse( {"message": "KEY_ERROR"}, status=400)
-        ##2. email이나 password 어떤 하나라도 잘못된 입력시 401  {"message": "INVALID_USER"},  {"message": "INVALID_Email"}
-        #존재하지 않는 이메일인 겨우
-        #이메일은 맞지만 비번이 잘못된 경우
-        if User.objects.get(headline__exact=data['email']).exists() :
-            if data['password'] == User.objects.get(headline__exact=data['email'])['password'] :
+            
+        if User.objects.filter(email=data['email']).exists() :
+            login_info = User.objects.get(email__exact=data['email'])
+            print(login_info)
+            if login_info.password == data['password']:
                 return JsonResponse({'message' : 'SUCCESS'}, status=201)
             else :
-                return JsonResponse({"message": "INVALID_USER"}, status = 401)        
-        ##3. 올바른 입력인 경우 -> {'message' : 'SUCCESS'} 로그인 성공 200
+                return JsonResponse({"message": "INVALID_USER"}, status = 401)
         else :
             return JsonResponse({"message": "INVALID_EMAIL"}, status = 401)
