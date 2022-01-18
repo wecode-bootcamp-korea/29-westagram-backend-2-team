2ee478b9ed5c5dd12dd9c7d1934bb622faea7d7b
OolongTea620 <rlafls9596@gmail.com>
2022-01-18 18:32:24 +0900

    sub import

--- students/hyelin/westargram/users/views.py
+++ students/hyelin/westargram/users/views.py
@@ -1,5 +1,4 @@
 import json
-from logging import exception 
 import re
 
 from django.views import View
@@ -83,7 +82,6 @@ class LoginView(View):
                 return JsonResponse({'message' : 'SUCCESS'}, status = 201)
             else :
                 return JsonResponse({"message": "INVALID_USER"}, status = 401)
-        
         except KeyError :
             return JsonResponse({"message": "KEY_ERROR"}, status = 400)
         except ValueError :
