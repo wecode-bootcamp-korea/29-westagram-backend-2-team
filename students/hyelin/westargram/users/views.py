bf4abfd29c81bc44eaa3489aedd4b04c0741cf5a
OolongTea620 <rlafls9596@gmail.com>
2022-01-18 17:57:55 +0900

    add try-except

--- students/hyelin/westargram/users/views.py
+++ students/hyelin/westargram/users/views.py
@@ -1,4 +1,5 @@
-import json 
+import json
+from logging import exception 
 import re
 
 from django.views import View
@@ -30,23 +31,23 @@ class SignUpView (View):
         REX_EMAIL   = r'^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
         REX_PASS    = r'^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$'
         
-        ## 1. name누락 
         if 'name' not in user_data :
             return JsonResponse({'message' : '이름을 입력해 주세요'}, status=400)
-        ## 2. email 누락 
+
         if 'email' not in user_data :
             return JsonResponse({'message' : '이메일을 입력해 주세요'}, status=400)
-        ## 3. password 누락 
+
         if 'password' not in user_data :
             return JsonResponse({'message': '비밀번호를 입력해 주세요'}, status=400)
-        ## 4. 이메일 양식 비충족
+
         if not re.fullmatch(REX_EMAIL, user_data['email']) :
             return JsonResponse({'message' : '유효하지 않은 이메일입니다'})
-        ## 5. password 양식 비충족 
+
         if not re.fullmatch(REX_PASS, user_data['password']) :
             return JsonResponse({'message' : '잘못된 형식의 비밀번호 입니다'}, status=401)
-        ## 6. 이메일 중복시 -> 이미 가입된 email입니다
+
         if not User.objects.filter(email = user_data['email']).exists() :
+            
             User.objects.create(
                 name = user_data['name'],
                 password = user_data['password'],
@@ -75,17 +76,15 @@ class LoginView(View):
             "message" : "SUCCESS" , status=201
         }
         """
-
-        data = json.loads(request.body)
-
-        if 'email' not in data  or 'password' not in data :
-            return JsonResponse( {"message": "KEY_ERROR"}, status = 400)
-            
-        if User.objects.filter(email=data['email']).exists() :
+        try :
+            data = json.loads(request.body)
             user = User.objects.get(email__exact=data['email'])
             if user.password == data['password']:
                 return JsonResponse({'message' : 'SUCCESS'}, status = 201)
             else :
                 return JsonResponse({"message": "INVALID_USER"}, status = 401)
-        else :
-            return JsonResponse({"message": "INVALID_EMAIL"}, status = 401)
+        
+        except KeyError :
+            return JsonResponse({"message": "KEY_ERROR"}, status = 400)
+        except ValueError :
+            return JsonResponse({"message": "INVALID_USER"}, status = 401)
