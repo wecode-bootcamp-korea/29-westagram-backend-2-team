e063abce636662489e0f04fddd4c8b793504b580
Jiyeon Kim <jiyeon.kim.ds@gmail.com>
2022-01-21 12:40:35 +0900

    [Add] add comment

--- students/jiyeon/postings/models.py
+++ students/jiyeon/postings/models.py
@@ -9,3 +9,13 @@ class Posting(models.Model):
 
     class Meta:
         db_table = 'postings'
+
+class Comment(models.Model):
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    post = models.ForeignKey(Posting, on_delete=CASCADE)
+    comment = models.CharField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'comments'
--- students/jiyeon/westagram/urls.py
+++ students/jiyeon/westagram/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from users.views import SignupView, LoginView
-from postings.views import PostingView
+from postings.views import PostingView, CommentView
         
 urlpatterns = [
     path('signup', SignupView.as_view()),
     path('login', LoginView.as_view()),
-    path('post', PostingView.as_view())
+    path('posting', PostingView.as_view()),
+    path('comment', CommentView.as_view())
 ]
